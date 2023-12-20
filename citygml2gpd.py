84a4e222ac4606c52af7fc0ee1c1ff747840df29
schmorri <137684094+schmorri@users.noreply.github.com>
2023-12-20 11:40:57 +0100

    updated citygml2gpd script

--- citygml2gpd.py
+++ citygml2gpd.py
@@ -1,108 +1,128 @@
-from lxml import etree
+import xml.etree.ElementTree as ET
 import geopandas as gpd
 from shapely.geometry import Polygon
 import pandas as pd
-from xml.etree import ElementTree
 
-pathToGml = r"<ENTER PATH TO GML HERE>"
+def parseGml(filePath: str) -> ET.ElementTree:
+    """Reads GML File and returns it as an Element Tree."""
+    eTree = ET.parse(filePath)
+    rootNode = eTree.getroot()
+    return rootNode
 
-def gmlToShapelyPolygon(gml_polygon: str) -> Polygon:
-    #print(gml_polygon)
+def findAllInETree(eTree: ET.ElementTree, query: str) -> list:
+    """Finds all Nodes based on a query string and returns a list of all finds."""
+    finds = eTree.findall(query)
+    return finds
+
+def posListToShapelyPolygon(posList: str) -> Polygon:
+    """Converts PosList string into shapely Polygon."""
+    if posList is not None:
+        coords = posList.split()
+        coordinates = [(float(coords[i]), float(coords[i + 1]), float(coords[i + 2])) for i in range(0, len(coords), 3)]
+
+    # Create a Shapely Polygon
+    shapely_polygon = Polygon(coordinates)
+
+    return shapely_polygon
+
+def addRowToGeoDataFrame(geoDataFrame: gpd.GeoDataFrame, newRow: dict) -> gpd.GeoDataFrame:
+    """Adds a new row to an existing GeoDataFrame."""
+    new_gdf = gpd.GeoDataFrame([newRow], geometry='geometry', crs=geoDataFrame.crs)
+    geoDataFrame = gpd.GeoDataFrame(pd.concat([geoDataFrame, new_gdf], ignore_index=True), crs=geoDataFrame.crs)
+    return geoDataFrame
+
+def extractHeightsAndGeometryFromGML(eTree: ET.ElementTree, geoDataFrame: gpd.GeoDataFrame, gmlID: str, function: str, buildingPart: bool) -> gpd.GeoDataFrame:
+    """Extracts the height and Geometry from a building / roof surface."""
     
-    if gml_polygon == "N/A":
-        return None
-    else:
-        # Parse the GML string
-        root = ElementTree.fromstring(gml_polygon)
+    heights = findAllInETree(eTree, ".//{http://www.opengis.net/citygml/building/1.0}measuredHeight")
+    if len(heights) == 0: print(f"no height for {gmlID}")
+    elif len(heights) > 1: print(f"height on {gmlID} might be lost...") 
+    height = float(heights[0].text)
 
-        # Extract coordinates from the GML
-        coordinates = []
-        pos_list = root.find(".//{http://www.opengis.net/gml}posList")
-        if pos_list is not None:
-            coords = pos_list.text.split()
-            coordinates = [(float(coords[i]), float(coords[i + 1]), float(coords[i + 2])) for i in range(0, len(coords), 3)]
-
-        # Create a Shapely Polygon
-        shapely_polygon = Polygon(coordinates)
-
-        return shapely_polygon
-
-def addGmlToGeoDataFrame(gdf: gpd.GeoDataFrame, gmlId: str, height: float, function: str, buildingPart: bool, geometry: str):
-    newRow = {"gml_id": gmlId, "height": height, "function": function, "buildingPart": buildingPart, "geometry": gmlToShapelyPolygon(geometry)}
-    new_gdf = gpd.GeoDataFrame([newRow], geometry='geometry', crs=gdf.crs)
-    return gpd.GeoDataFrame(pd.concat([gdf, new_gdf], ignore_index=True), crs=gdf.crs)
-
-def extract_building_parts_with_geometry(citygml_file):
-    data = gpd.GeoDataFrame(
-        columns = [
-            "gml_id",
-            "height",
-            "function",
-            "buildingPart"
-        ],
-        geometry= [],
-        crs="EPSG:25832"
-    )
-    
-    tree = etree.parse(citygml_file)
-    root = tree.getroot()
-
-    # Extract namespaces from the root element
-    nsmap = root.nsmap
-
-    # Define namespaces for XPath queries
-    ns = {
-        'core': nsmap.get('core', ''),
-        'gml': nsmap.get('gml', ''),
-        'bldg': nsmap.get('bldg', ''),
-    }
-
-    # Find all Building elements
-    building_elements = root.findall('.//bldg:Building', namespaces=ns)
-
-    for building_element in building_elements:
-        gml_id = building_element.get('{http://www.opengis.net/gml}id')
-
-        # Check if measuredHeight element exists
-        measured_height_element = building_element.find('./bldg:measuredHeight', namespaces=ns)
-        measured_height = measured_height_element.text if measured_height_element is not None else "N/A"
-
-        function_element = building_element.find('./bldg:function', namespaces=ns)
-        function = function_element.text if function_element is not None else "N/A"
-
-        # Extract 2D geometry
-        geometry_element = building_element.find('.//gml:Polygon', namespaces=ns)
-        if geometry_element is not None:
-            geometry_text = etree.tostring(geometry_element, pretty_print=True).decode()
-        else:
-            geometry_text = "N/A"
-
-        data = addGmlToGeoDataFrame(data, gml_id, measured_height, function, False, geometry_text)
-
-        # Find all BuildingPart elements within the current Building
-        building_part_elements = building_element.findall('.//bldg:BuildingPart', namespaces=ns)
-
-        for building_part_element in building_part_elements:
-            part_gml_id = building_part_element.get('{http://www.opengis.net/gml}id')
-
-            part_measured_height_element = building_part_element.find('./bldg:measuredHeight', namespaces=ns)
-            part_measured_height = part_measured_height_element.text if part_measured_height_element is not None else "N/A"
-
-            part_function_element = building_part_element.find('./bldg:function', namespaces=ns)
-            part_function = part_function_element.text if part_function_element is not None else "N/A"
-
-            # Extract 2D geometry for BuildingPart
-            part_geometry_element = building_part_element.find('.//gml:Polygon', namespaces=ns)
-            if part_geometry_element is not None:
-                part_geometry_text = etree.tostring(part_geometry_element, pretty_print=True).decode()
+    roofSurfaces = findAllInETree(eTree, ".//{http://www.opengis.net/citygml/building/1.0}RoofSurface")
+    if len(roofSurfaces) == 0: 
+        print(f"no roofSurfaces for {gmlID}")
+    elif len(roofSurfaces) > 1: 
+        for rs in roofSurfaces:
+            posLists = findAllInETree(rs, ".//{http://www.opengis.net/gml}posList")
+            if len(posLists) == 0:
+                print(f"no geometry for {gmlID}")
+            elif len(posLists) == 1:
+                geoDataFrame = addRowToGeoDataFrame(geoDataFrame, newRow = {
+                    "gml_id": gmlID, 
+                    "height": height, 
+                    "function": function, 
+                    "buildingPart": buildingPart,
+                    "geometry": posListToShapelyPolygon(posLists[0].text)
+                })
             else:
-                part_geometry_text = "N/A"
+                for pl in posLists:
+                    geoDataFrame = addRowToGeoDataFrame(geoDataFrame, newRow = {
+                        "gml_id": gmlID, 
+                        "height": height, 
+                        "function": function, 
+                        "buildingPart": buildingPart,
+                        "geometry": posListToShapelyPolygon(pl.text)
+                    })
+    else:
+        posLists = findAllInETree(roofSurfaces[0], ".//{http://www.opengis.net/gml}posList")
+        if len(posLists) == 0:
+            print(f"no geometry for {gmlID}")
+        elif len(posLists) == 1:
+            geoDataFrame = addRowToGeoDataFrame(geoDataFrame, newRow = {
+                "gml_id": gmlID, 
+                "height": height, 
+                "function": function, 
+                "buildingPart": buildingPart,
+                "geometry": posListToShapelyPolygon(posLists[0].text)
+            })
+        else:
+            for pl in posLists:
+                geoDataFrame = addRowToGeoDataFrame(geoDataFrame, newRow = {
+                    "gml_id": gmlID, 
+                    "height": height, 
+                    "function": function, 
+                    "buildingPart": buildingPart,
+                    "geometry": posListToShapelyPolygon(pl.text)
+                })
+    
+    return geoDataFrame
 
-            data = addGmlToGeoDataFrame(data, gml_id, part_measured_height, part_function, True, part_geometry_text)
-
-    return data
+def saveGeoDataFrameAsShape(filePath: str, geoDataFrame: gpd.GeoDataFrame): 
+    geoDataFrame.to_file(filePath)   
 
 if __name__ == "__main__":
-    citygml_file_path = pathToGml
-    data = extract_building_parts_with_geometry(citygml_file_path)
-    print(data)
+    filePath = r"C:\Users\alexa\Documents\GitHub\Preprocessing4PALM\LoD2_32_376_5705_1_NW.gml"
+    rootNodeGml = parseGml(filePath)
+
+    data = gpd.GeoDataFrame(
+        columns=["gml_id", "height", "function", "buildingPart"],
+        geometry=[],
+        crs="EPSG:25832",
+    )
+
+    buildings = findAllInETree(rootNodeGml, ".//{http://www.opengis.net/citygml/building/1.0}Building")
+
+    for building in buildings:
+
+        gmlID = building.attrib['{http://www.opengis.net/gml}id']
+        function = None
+        height = None
+        buildingPart = None
+        geometry = None
+
+        buildingParts = findAllInETree(building, ".//{http://www.opengis.net/citygml/building/1.0}BuildingPart")
+
+        functions = findAllInETree(building, ".//{http://www.opengis.net/citygml/building/1.0}function")
+        
+        if len(functions) == 0: print(f"no function for {gmlID}")
+        elif len(functions) > 1: print(f"function on {gmlID} might be lost...") 
+        function = functions[0].text
+
+        if len(buildingParts) == 0:
+            data = extractHeightsAndGeometryFromGML(building, data, gmlID, function, False)
+        else:
+            for bp in buildingParts:
+                data = extractHeightsAndGeometryFromGML(bp, data, gmlID, function, True)
+
+saveGeoDataFrameAsShape(r"C:\Users\alexa\Documents\GitHub\Preprocessing4PALM\shape\data_new.shp")
